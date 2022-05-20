6d8424969db838412d1fa7841f9e29616855d6b4
Oliver Fabel <oliver.fabel@bsi-software.com>
2022-05-20 11:52:32 +0200

    add website template

---
+++ templates/landingpage/design.js
@@ -0,0 +1,7 @@
+const {cx, SchemaVersion} = require('@bsi-cx/design-build');
+
+module.exports = cx.design
+  .withTitle('Landingpage Scaffold Template')
+  .withAuthor('BSI Software AG')
+  .withDate('26.11.2021')
+  .withPreviewImage(require('./thumbnail.png'));
---
+++ templates/website/properties.js
@@ -0,0 +1,3 @@
+module.exports = {
+  title: 'BSI CX Scaffold Template - Website'
+};
