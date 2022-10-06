9518d1b20042d3ec279a1b86632ded646621ee29
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2022-10-06 16:47:04 +0200

    small story and small section

---
+++ templates/email/content-elements/sections/small-event/index.js
@@ -0,0 +1,22 @@
+const { cx, Icon } = require("@bsi-cx/design-build");
+
+module.exports = cx.contentElement
+  .withElementId("small-event")
+  .withIcon(Icon.TWO_COLUMNS)
+  .withLabel("Small Event")
+  .withFile(require("./template.twig"))
+  .withParts(
+    cx.part.plainText
+      .withId("3a32c5f4-457c-11ed-b878-0242ac120002")
+      .withLabel("Date-1"),
+    cx.part.plainText
+      .withId("3a32c4b4-457c-11ed-b878-0242ac120002")
+      .withLabel("Date-2"),
+    cx.part.plainText
+      .withId("3a32c36a-457c-11ed-b878-0242ac120002")
+      .withLabel("Date-3"),
+    cx.part.formattedText
+      .withId("3a32c1ee-457c-11ed-b878-0242ac120002")
+      .withLabel("Text Link")
+      .withHtmlEditorConfig(require("../../../../formatted-text-config"))
+  );
