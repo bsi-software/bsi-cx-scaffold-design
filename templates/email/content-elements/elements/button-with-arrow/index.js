37fa45fcbe4259c4ff6ee0dd34560e92be259c10
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2022-11-02 08:52:09 +0100

    fix white line in footer

---
+++ templates/email/content-elements/elements/button-with-arrow/index.js
@@ -0,0 +1,19 @@
+const {
+    cx,
+    Icon,
+} = require("@bsi-cx/design-build");
+
+module.exports =
+    cx.contentElementPart
+        .withElementId(
+            "41a651ec-56a3-11ed-9b6a-0242ac120002",
+        )
+        .withIcon(
+            Icon.TEXT,
+        )
+        .withLabel(
+            "Button With Arrow ",
+        )
+        .withFile(
+            require("./template.twig"),
+        );
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,74 @@ const {
   DesignType,
 } = require("@bsi-cx/design-build");
 
-module.exports = WebpackConfigBuilder.fromConfigs(
+// module.exports =
+//   WebpackConfigBuilder.fromConfigs(
+//     new BuildConfig()
+//       .withName(
+//         "email",
+//       )
+//       .withVersion(
+//         "1.0.0",
+//       )
+//       .withDesignType(
+//         DesignType.EMAIL,
+//       )
+//       .withTargetVersion(
+//         Version.CX_1_3,
+//       )
+//       .withRootPath(
+//         path.resolve(
+//           __dirname,
+//           "templates",
+//           "email",
+//         ),
+//       )
+//       .withPropertiesFilePath(
+//         "properties.js",
+//       ),
+//   );
+
+const emailBuildConfig =
   new BuildConfig()
-    .withName("email")
-    .withVersion("1.0.0")
-    .withDesignType(DesignType.EMAIL)
-    .withTargetVersion(Version.CX_1_3)
-    .withRootPath(path.resolve(__dirname, "templates", "email"))
-    .withPropertiesFilePath("properties.js")
-);
+    .withName(
+      "email",
+    )
+    .withVersion(
+      "1.0.0",
+    )
+    .withDesignType(
+      DesignType.EMAIL,
+    )
+    .withTargetVersion(
+      Version.CX_1_3,
+    )
+    .withRootPath(
+      path.resolve(
+        __dirname,
+        "templates",
+        "email",
+      ),
+    )
+    .withPropertiesFilePath(
+      "properties.js",
+    );
+
+module.exports =
+  WebpackConfigBuilder.fromConfigs(
+    emailBuildConfig
+      .clone()
+      .withName(
+        "master-template-cx-22.0-email-de",
+      )
+      .withPropertiesFilePath(
+        "properties-de.js",
+      ),
+    emailBuildConfig
+      .clone()
+      .withName(
+        "master-template-cx-22.0-email-en",
+      )
+      .withPropertiesFilePath(
+        "properties-en.js",
+      ),
+  );
